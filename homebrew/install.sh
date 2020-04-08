046a80f94c48ae0e66cf04f688ac3b2de50a30b1
Denis Davydenko <iamstylle@gmail.com>
2020-04-08 16:51:58 +0300

    Fixed brew install command

--- homebrew/install.sh
+++ homebrew/install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 set -e
-[ -z $(which brew) ] && /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+[ -z $(which brew) ] && /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
 [ -z $(which zsh) ] && brew install zsh
 
